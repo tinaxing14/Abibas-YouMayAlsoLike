8570c0e6fae5da6d3b184accb215168ff7526660
Tina Xing <tinaxing14@gmail.com>
2020-05-19 18:54:09 -0400

    add additem and deleteitem file

---
+++ server/routes/additem.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const db = require('../db');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  //TODO: takes a product information and add the product in database
+});
+
+module.exports = router;
