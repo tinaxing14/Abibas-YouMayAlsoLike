ebd3f390f53c10771d03f3f9eaeef36b218f2606
Tina Xing <tinaxing14@gmail.com>
2020-05-26 18:55:07 -0400

    complete addProduct and deletProduct psql model

--- data/Postgres/model.js
+++ data/Postgres/model.js
@@ -28,8 +28,48 @@ module.exports = {
         });
     });
   },
+  deleteProduct: (id, callback) => {
+    pool.connect().then((client) => {
+      return client
+              .query(`delete from shoes where id = '${id}'`)
+              .then((res) => {
+                client.release();
+                console.log('product deleted', res)
+                callback(null, res.rows[0]);
+              })
+              .catch((err) => {
+                client.release();
+                console.log(err.stack);
+                callback(err);
+              })
+    });
+  },
+
+  addProduct: (option, callback) => {
+    const { title, images, price, href } = option;
+    const related_products = option.related_products.replace("[", "{").replace("]", "}");
+    const sql = `insert into shoes (title, images, price, href, related_products) values ('${title}', '${images}','${price}','${href}','${related_products}') returning id`;
+    console.log(sql)
+    pool.connect().then((client) => {
+      return client
+              .query(sql)
+              .then((res) => {
+                client.release();
+                callback(null, res.rows[0]);
+              })
+              .catch((err) => {
+                client.release();
+                console.log(err.stack);
+                callback(err);
+              })
+    });
+  }
+
 };
 
-console.log(module.exports.getProduct(5, (err, result) => {
-  console.log(result)
-}))
+
+
+
+// console.log(module.exports.getProduct(5, (err, result) => {
+//   console.log(result)
+// }))
