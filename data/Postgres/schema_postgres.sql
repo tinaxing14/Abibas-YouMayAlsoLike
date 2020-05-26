cae8515ae3c9768e71802e8c5935569aa065a428
Tina Xing <tinaxing14@gmail.com>
2020-05-25 22:04:39 -0400

    correct the typo in schema

--- data/Postgres/schema_postgres.sql
+++ data/Postgres/schema_postgres.sql
@@ -1,4 +1,4 @@
-
+\c abibas;
 
 DROP TABLE IF EXISTS shoes;
 DROP TABLE IF EXISTS users;
@@ -6,12 +6,12 @@ DROP TABLE IF EXISTS user_likes;
 
 CREATE TABLE shoes (
   id serial primary key,
-  shoe_id varchar(10) not null,
+  shoe_id varchar(10),
   title varchar(60),
   images varchar(250),
   price smallint,
   href varchar(250),
-  related_produtcs varchar(10) ARRAY[12]
+  related_products varchar(10) ARRAY[12]
 );
 
 CREATE TABLE users (
@@ -24,3 +24,8 @@ CREATE TABLE user_likes (
   users_id int references users(id) on delete cascade,
   shoes_id int references shoes(id) on delete cascade
 );
+
+
+\copy users(users_name) from '/Users/TinaXING/Downloads/data/users.csv' delimiter ',' csv;
+\copy shoes(shoe_id, title, images, price, href, related_products) from '/Users/TinaXING/Downloads/data/shoes.csv' delimiter ',' csv header;
+\copy user_likes(users_id, shoes_id) from '/Users/TinaXING/Downloads/data/userlikes.csv' delimiter ',' csv header;
