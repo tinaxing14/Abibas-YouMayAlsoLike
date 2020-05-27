a0e026f21fcff0238b47aa9c51aa81058bfecfa0
Tina Xing <tinaxing14@gmail.com>
2020-05-27 13:18:44 -0400

    start working on Mongo model

---
+++ data/Mongo/model.js
@@ -0,0 +1,26 @@
+
+const { User, Shoe } = require('./mongodb_schema.js');
+
+module.exports = {
+  getProduct: (id, callback) => {
+    Shoe.findById(
+  },
+
+  deleteProduct: () => {
+
+  },
+
+  addProduct: () => {
+
+  },
+
+  likeProduct: () => {
+
+  },
+
+  updateProduct: () => {
+
+  },
+
+
+}
