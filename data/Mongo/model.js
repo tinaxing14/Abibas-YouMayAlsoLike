7d1f9821452c9fe92f62acbc67fca1d1b862a13d
Tina Xing <tinaxing14@gmail.com>
2020-05-27 20:56:41 -0400

    complete mongo model

--- data/Mongo/model.js
+++ data/Mongo/model.js
@@ -3,24 +3,80 @@ const { User, Shoe } = require('./mongodb_schema.js');
 
 module.exports = {
   getProduct: (id, callback) => {
-    Shoe.findById(
+    Shoe.findOne({id: id}, (err, results) => {
+      if(err) {
+        console.log(err);
+      } else {
+        callback(null, results);
+      }
+    });
   },
 
-  deleteProduct: () => {
-
+  deleteProduct: (id, callback) => {
+    Shoe.deleteOne({id: id}, (err, results) => {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log(results);
+        callback(null, results);
+      }
+    });
   },
 
-  addProduct: () => {
-
+  addProduct: (option, callback) => {
+    const { id, title, images, price, href, relatedProducts } = option;
+    const shoe = [{
+      id: id,
+      title: title,
+      images: images,
+      price: price,
+      href: href,
+      relatedProducts: relatedProducts
+    }]
+    Shoe.insertMany(shoe, (err, results) => {
+      if(err){
+        console.log(err);
+      } else {
+        callback(null, results);
+      }
+    } )
+    
   },
 
-  likeProduct: () => {
+  likeProduct: (ids, callback) => {
+    const { id, userid } = ids
+    User.findOne({userName: userid}, (err, result) => {
+      const arr = [...result.likedShoes, id];
+      User.findOneAndUpdate({userName: userid}, {likedShoes: arr}, (err, results) => {
+        if(err) {
+          console.log(err)
+        } else {
+          callback(null, results);
+        }
+      })
+    })
 
+  
   },
 
-  updateProduct: () => {
-
+  updateProduct: (option, id, callback) => {
+    const { title, images, price, href, relatedProducts } = option;
+    const shoe = {
+      id:id,
+      title: title,
+      images: images,
+      price: price,
+      href: href,
+      relatedProducts: relatedProducts
+    };
+    Shoe.findOneAndReplace({id: id}, shoe, (err, results) => {
+      if(err){
+        console.log(err);
+      } else {
+        callback(null, results);
+      }
+    })
   },
-
 
 }
+
