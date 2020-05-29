0213c731cda6a16d77e56dde7bba7d38fc6f236f
Tina Xing <tinaxing14@gmail.com>
2020-05-29 19:05:16 -0400

    modify likeproduct function to remove product when user unlike them

--- data/Mongo/model.js
+++ data/Mongo/model.js
@@ -1,10 +1,9 @@
-
-const { User, Shoe } = require('./mongodb_schema.js');
+const { User, Shoe } = require("./mongodb_schema.js");
 
 module.exports = {
   getProduct: (id, callback) => {
-    Shoe.findOne({id: id}, (err, results) => {
-      if(err) {
+    Shoe.findOne({ id: id }, (err, results) => {
+      if (err) {
         console.log(err);
       } else {
         callback(null, results);
@@ -13,8 +12,8 @@ module.exports = {
   },
 
   deleteProduct: (id, callback) => {
-    Shoe.deleteOne({id: id}, (err, results) => {
-      if(err) {
+    Shoe.deleteOne({ id: id }, (err, results) => {
+      if (err) {
         console.log(err);
       } else {
         console.log(results);
@@ -25,58 +24,67 @@ module.exports = {
 
   addProduct: (option, callback) => {
     const { id, title, images, price, href, relatedProducts } = option;
-    const shoe = [{
-      id: id,
-      title: title,
-      images: images,
-      price: price,
-      href: href,
-      relatedProducts: relatedProducts
-    }]
+    const shoe = [
+      {
+        id: id,
+        title: title,
+        images: images,
+        price: price,
+        href: href,
+        relatedProducts: relatedProducts,
+      },
+    ];
     Shoe.insertMany(shoe, (err, results) => {
-      if(err){
+      if (err) {
         console.log(err);
       } else {
         callback(null, results);
       }
-    } )
-    
+    });
   },
 
   likeProduct: (ids, callback) => {
-    const { id, userid } = ids
-    User.findOne({userName: userid}, (err, result) => {
-      const arr = [...result.likedShoes, id];
-      User.findOneAndUpdate({userName: userid}, {likedShoes: arr}, (err, results) => {
-        if(err) {
-          console.log(err)
-        } else {
-          callback(null, results);
+    const { id, userid } = ids;
+    User.findOne({ userName: userid }, (err, result) => {
+      console.log('origin arr', result.likedShoes)
+      let index = result.likedShoes.indexOf(id);
+      let arr;
+      if (index < 0) {
+        arr = [...result.likedShoes, id];
+      } else {
+        arr = result.likedShoes.slice()
+        arr.splice(index, 1)
+      }
+      User.findOneAndUpdate(
+        { userName: userid },
+        { likedShoes: arr },
+        (err, results) => {
+          if (err) {
+            console.log(err);
+          } else {
+            callback(null, results);
+          }
         }
-      })
-    })
-
-  
+      );
+    });
   },
 
   updateProduct: (option, id, callback) => {
     const { title, images, price, href, relatedProducts } = option;
     const shoe = {
-      id:id,
+      id: id,
       title: title,
       images: images,
       price: price,
       href: href,
-      relatedProducts: relatedProducts
+      relatedProducts: relatedProducts,
     };
-    Shoe.findOneAndReplace({id: id}, shoe, (err, results) => {
-      if(err){
+    Shoe.findOneAndReplace({ id: id }, shoe, (err, results) => {
+      if (err) {
         console.log(err);
       } else {
         callback(null, results);
       }
-    })
+    });
   },
-
-}
-
+};
